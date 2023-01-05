9c645f8b0483e86530a99f74963ab887c840b9bb
pavel.kuznetsov <pavel@hypertrack.io>
2023-01-05 16:53:16 +0200

    ThreadPool 1

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ use test_rust::thread_pool::ThreadPoolExecutor;
 
 fn main() {
     println!("-----------------");
-
     let mut rng = Box::new(rand::thread_rng());
     let thread_pool = MyThreadPoolExecutor::new(5);
 
-    for i in 0..10 {
+    let task_number = 10;
+    for i in 0..task_number {
         let val: f64 = rng.gen();
-        println!("Run {}", val);
+        println!("Run {} {}", i, val);
         thread_pool.run(Box::new(move || {
             println!("Start {}", val);
             thread::sleep(Duration::from_secs_f64(val));
@@ -26,7 +26,10 @@ fn main() {
         }))
     }
 
-    for res in thread_pool.results() {
-        println!("Result {}", res);
+    for (index, res) in thread_pool.results().enumerate() {
+        println!("Result {} {}", index, res);
+        if index == task_number - 1 {
+            break;
+        };
     };
 }
--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,6 +1,7 @@
-use rand::{Rng, rngs::ThreadRng};
-use std::thread::{self, JoinHandle};
+use rand::{rngs::ThreadRng, Rng};
 use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
 pub trait ThreadPoolExecutor<Result> {
     type F: FnOnce() -> Result;
@@ -8,34 +9,59 @@ pub trait ThreadPoolExecutor<Result> {
     fn results(&self) -> mpsc::Iter<'_, Result>;
 }
 
-pub struct MyThreadPoolExecutor<T> {
+pub struct MyThreadPoolExecutor<T> where T: Send + Sync {
     rng: ThreadRng,
     threads: Vec<JoinHandle<()>>,
-    rx: Receiver<T>,
-    tx: Sender<T>
+    task_tx: Sender<Box<dyn FnOnce() -> T + Send + Sync>>,
+    task_rx: Arc<Mutex<Receiver<Box<dyn FnOnce() -> T + Send + Sync>>>>,
+    result_tx: Arc<Mutex<Sender<T>>>,
+    result_rx: Receiver<T>
 }
 
-impl <T> MyThreadPoolExecutor<T> {
+impl<T: 'static> MyThreadPoolExecutor<T> where T: Send + Sync {
     pub fn new(threads_num: i32) -> Self {
-        let (tx, rx) = mpsc::channel();
+        let (result_tx, result_rx) = mpsc::channel();
+        let (
+            task_tx, 
+            task_rx
+        ) = mpsc::channel();
+        let task_rx_link: Arc<Mutex<Receiver<Box<dyn FnOnce() -> T + Send + Sync>>>> = 
+            Arc::new(Mutex::new(task_rx));
+        let result_tx_link =
+            Arc::new(Mutex::new(result_tx));
         return MyThreadPoolExecutor {
             rng: rand::thread_rng(),
-            threads: (0..threads_num).map(|_| thread::spawn(|| {})).collect(),
-            rx,
-            tx
+            threads: (0..threads_num)
+                .map(|_| {
+                    let task_rx = Arc::clone(&task_rx_link);
+                    let result_tx = Arc::clone(&result_tx_link);
+                    thread::spawn(move || {
+                        loop {
+                            let task: Box<dyn FnOnce() -> T + Send + Sync> = task_rx.lock().unwrap().recv().unwrap();
+                            let result = task();
+                            result_tx.lock().unwrap().send(result).unwrap();
+                        }
+                    })
+                })
+                .collect(),
+                task_tx,
+                task_rx: task_rx_link,
+                result_tx: result_tx_link,
+                result_rx
         };
     }
 }
 
-impl<T> ThreadPoolExecutor<T> for MyThreadPoolExecutor<T> {
-    type F = Box<dyn FnOnce() -> T>;
-    fn run(&self, code: Self::F) {
-        let result = code();
-        self.tx.send(result).unwrap();
+impl<T> ThreadPoolExecutor<T> for MyThreadPoolExecutor<T>
+where
+    T: Send + Sync + 'static,
+{
+    type F = Box<dyn FnOnce() -> T + Send + Sync>;
+    fn run(&self, task: Self::F) {
+        self.task_tx.send(Box::new(task)).unwrap();
     }
 
     fn results(&self) -> mpsc::Iter<'_, T> {
-        self.rx.iter()
+        self.result_rx.iter()
     }
 }
-
